62697dee05d58071a089012a66907185b6fcd4e8
yyl <yuanyu.lin@gmail.com>
2018-12-17 17:31:54 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -17,28 +17,36 @@ def set_global(args):
     global arch
     global src_usr_lib_dir
     global src_usr_bin_dir
+    global src_usr_share_dir
     global dst_lib_dir
     global dst_usr_bin_dir
     global src_include_dir
+    global tmp_include_dir
     global dst_include_dir
+    global dst_usr_share_dir
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
     arch = ops.getEnv("ARCH_ALT")
     if arch == "armhf":
         src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabihf")
         src_usr_bin_dir = iopc.getBaseRootFile("usr/bin")
+        src_usr_share_dir = iopc.getBaseRootFile("usr/share")
     elif arch == "armel":
         src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabi")
         src_usr_bin_dir = iopc.getBaseRootFile("usr/bin")
+        src_usr_share_dir = iopc.getBaseRootFile("usr/share")
     elif arch == "x86_64":
         src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/x86_64-linux-gnu")
         src_usr_bin_dir = iopc.getBaseRootFile("usr/bin")
+        src_usr_share_dir = iopc.getBaseRootFile("usr/share")
     else:
         sys.exit(1)
     dst_lib_dir = ops.path_join(output_dir, "lib")
     dst_usr_bin_dir = ops.path_join(output_dir, "usr/bin")
+    dst_usr_share_dir = ops.path_join(output_dir, "usr/share")
 
-    src_include_dir = iopc.getBaseRootFile("usr/include/luajit-2.0")
+    src_include_dir = iopc.getBaseRootFile("usr/include")
+    tmp_include_dir = ops.path_join(output_dir, ops.path_join("include",args["pkg_name"]))
     dst_include_dir = ops.path_join("include",args["pkg_name"])
 
 
@@ -49,30 +57,15 @@ def MAIN_ENV(args):
 def MAIN_EXTRACT(args):
     set_global(args)
 
-    if False :
-        ops.mkdir(dst_usr_bin_dir)
-        ops.copyto(ops.path_join(src_usr_bin_dir, "luajit-2.0.4"), dst_usr_bin_dir)
-        ops.ln(dst_usr_bin_dir, "luajit-2.0.4", "luajit")
+    ops.mkdir(dst_usr_bin_dir)
+    ops.copyto(ops.path_join(src_usr_bin_dir, "luajit-2.0.4"), dst_usr_bin_dir)
+    ops.ln(dst_usr_bin_dir, "luajit-2.0.4", "luajit")
         
-        ops.mkdir(dst_lib_dir)
-        ops.copyto(ops.path_join(src_usr_lib_dir, "libluajit-5.1.so.2.0.4"), dst_lib_dir)
-        ops.ln(dst_lib_dir, "libluajit-5.1.so.2.0.4", "libluajit-5.1.so.2.0")
-        ops.ln(dst_lib_dir, "libluajit-5.1.so.2.0.4", "libluajit-5.1.so.2")
-        ops.ln(dst_lib_dir, "libluajit-5.1.so.2.0.4", "libluajit-5.1.so")
-        ops.ln(dst_lib_dir, "libluajit-5.1.so.2.0.4", "liblua5.1.so.0")
-        ops.ln(dst_lib_dir, "libluajit-5.1.so.2.0.4", "liblua5.1.so")
+    ops.mkdir(dst_usr_share_dir)
+    ops.copyto(ops.path_join(src_usr_share_dir, "luajit-2.0.4"), dst_usr_share_dir)
 
-    ops.mkdir(dst_lib_dir)
-    ops.copyto(ops.path_join(src_usr_lib_dir, "liblua5.1.so.0.0.0"), dst_lib_dir)
-    ops.ln(dst_lib_dir, "liblua5.1.so.0.0.0", "liblua5.1.so.0.0")
-    ops.ln(dst_lib_dir, "liblua5.1.so.0.0.0", "liblua5.1.so.0")
-    ops.ln(dst_lib_dir, "liblua5.1.so.0.0.0", "liblua5.1.so")
-    #ops.mkdir(dst_include_dir)
-    #iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lauxlib.h"), dst_include_dir)
-    #iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lua.h"), dst_include_dir)
-    #iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "luaconf.h"), dst_include_dir)
-    #iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "luajit.h"), dst_include_dir)
-    #iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lualib.h"), dst_include_dir)
+    ops.mkdir(tmp_include_dir)
+    ops.copyto(ops.path_join(src_include_dir, 'luajit-2.0'), tmp_include_dir)
     return True
 
 def MAIN_PATCH(args, patch_group_name):
@@ -96,13 +89,20 @@ def MAIN_BUILD(args):
 def MAIN_INSTALL(args):
     set_global(args)
 
-    iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lauxlib.h"), dst_include_dir)
-    iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lua.h"), dst_include_dir)
-    iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "luaconf.h"), dst_include_dir)
-    iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "luajit.h"), dst_include_dir)
-    iopc.installBin(args["pkg_name"], ops.path_join(src_include_dir, "lualib.h"), dst_include_dir)
-    iopc.installBin(args["pkg_name"], ops.path_join(dst_lib_dir, "."), "lib") 
-    #iopc.installBin(args["pkg_name"], ops.path_join(dst_usr_bin_dir, "."), "usr/bin") 
+    iopc.installBin(args["pkg_name"], ops.path_join(dst_usr_share_dir, "."), "usr/share") 
+    iopc.installBin(args["pkg_name"], ops.path_join(dst_usr_bin_dir, "."), "usr/bin") 
+    iopc.installBin(args["pkg_name"], ops.path_join(tmp_include_dir, "."), dst_include_dir)
+    return False
+
+def MAIN_SDKENV(args):
+    set_global(args)
+
+    sdkinclude_dir = ops.path_join(iopc.getSdkPath(), 'usr/include/' + args["pkg_name"])
+    cflags = ""
+    cflags += " -I" + sdkinclude_dir
+    cflags += " -I" + ops.path_join(sdkinclude_dir, "luajit-2.0")
+    iopc.add_includes(cflags)
+
     return False
 
 def MAIN_CLEAN_BUILD(args):
